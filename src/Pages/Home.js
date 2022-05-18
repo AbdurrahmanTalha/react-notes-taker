cf0b153b087181dd31b01f753c0bb2bfdce4264e
AbdurrahmanTalha <66727914+AbdurrahmanTalha@users.noreply.github.com>
2022-05-18 21:54:43 +0600

    added login (design) and navbar

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const Home = () => {
+    return (
+        <div>
+            <Button type="primary">Home</Button>
+        </div>
+    );
+};
+
+export default Home;
